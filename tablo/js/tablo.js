fb6cf6ff277eb0597423cefbc632449d6a3a7a70
Ksenia V. Mamich <fenice@yandex-team.ru>
2015-08-28 18:42:35 +0300

    tablo 2/6

--- tablo/js/tablo.js
+++ tablo/js/tablo.js
@@ -1,15 +1,16 @@
 $(function() {
 
-    $('td.cell').hover(function() {
-        var classes = $(this).attr('class');
-        var columnClass = classes.replace(/.*(column__\w+).*/, '$1');
-        var rowClass = classes.replace(/.*(row__\w+).*/, '$1');
+    $('td.cell, th.cell').hover(function() {
+        var td = $(this);
+        var columnClass = td.attr('class').replace(/.*(column__\w+).*/, '$1');
+        var rowClass = td.parent().attr('class').replace(/.*(row__\w+).*/, '$1');
 
-        console.log(classes);
         console.log(columnClass);
         console.log(rowClass);
 
-        $('.' + columnClass).toggleClass('cell__highlighted');
+        var highlighted = '.cell.' + columnClass + ', .' + rowClass + ' td';
+        console.log(highlighted);
+        $(highlighted).toggleClass('cell__highlighted');
     });
 
 });
