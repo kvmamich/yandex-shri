85cc444edda16d6a1adb064207a34f5cfe24af1b
Ksenia V. Mamich <fenice@yandex-team.ru>
2015-08-28 19:02:19 +0300

    tablo 3/6

--- tablo/js/tablo.js
+++ tablo/js/tablo.js
@@ -4,13 +4,20 @@ $(function() {
         var td = $(this);
         var columnClass = td.attr('class').replace(/.*(column__\w+).*/, '$1');
         var rowClass = td.parent().attr('class').replace(/.*(row__\w+).*/, '$1');
+        $('.cell.' + columnClass + ', .' + rowClass + ' td').toggleClass('cell__highlighted');
+    });
 
-        console.log(columnClass);
-        console.log(rowClass);
 
-        var highlighted = '.cell.' + columnClass + ', .' + rowClass + ' td';
-        console.log(highlighted);
-        $(highlighted).toggleClass('cell__highlighted');
+    $(window).resize(function() {
+        var width = $(this).width();
+        if (width >= 1000) {
+            $('table.tablo').removeClass('size-s').removeClass('size-m').addClass('size-l');
+        } else if (width >= 800) {
+            $('table.tablo').removeClass('size-s').removeClass('size-l').addClass('size-m');
+        } else {
+            $('table.tablo').removeClass('size-m').removeClass('size-l').addClass('size-s');
+        }
+
     });
 
 });
